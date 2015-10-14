c4655b237eee9581b208646d1386343bc62f68ab
Adam White <adam.nathaniel.white@gmail.com>
2015-10-14 13:50:33 -0400

    Initial framework of methods

---
+++ P2.py
@@ -0,0 +1,28 @@
+def printSmallBoard(smallBoard):
+    print("not implemented")
+    assert(0)
+
+def printWholeBoard(board):
+    print("not implemented")
+    assert(0)
+
+def heuristic(board):
+    print("not implemented")
+    assert(0)
+
+def getSuccessors(board, move):
+    print("not implemented")
+    assert(0)
+
+def simulateMove(board, whichMiniGame, whosTurn, depth, alpha, beta):
+    
+
+def playGames():
+    opponentsTurn = False
+    while noOneWon:
+        if( opponentsTurn == True ):
+            #wait for command input from opponent
+            assert(0)
+        else if(opponentsTurn == False ):
+            #It's the computer's turn
+            assert(0)
