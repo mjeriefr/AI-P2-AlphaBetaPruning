d52318900a78cd079cc951dd62d17b4d62219b94
Adam White <adam.nathaniel.white@gmail.com>
2015-10-15 04:53:18 -0400

    Print board state (clearly understandable for user)

--- P2.py
+++ P2.py
@@ -1,10 +1,16 @@
-def printSmallBoard(smallBoard):
-    print("not implemented")
-    assert(0)
+def printMiniBoard(miniBoard):
+    print( miniBoard[0], " | ", miniBoard[1], " | ", miniBoard[2])
+    print( "---------" )
+    print( miniBoard[3], " | ", miniBoard[4], " | ", miniBoard[5])
+    print( "---------" )
+    print( miniBoard[6], " | ", miniBoard[7], " | ", miniBoard[8])
 
 def printWholeBoard(board):
-    print("not implemented")
-    assert(0)
+        for boardRow in range(0, 3):
+            for innerRow in range(0, 3):
+                print( board[(boardRow*3)+0][innerRow*3+0], " ", board[(boardRow*3)+0][innerRow*3+1], " ", board[(boardRow*3)+0][innerRow*3+2], " | ", board[(boardRow*3)+1][innerRow*3+0], " ", board[(boardRow*3)+1][innerRow*3+1], " ", board[(boardRow*3)+1][innerRow*3+2], " | ", board[(boardRow*3)+2][innerRow*3+0], " ", board[(boardRow*3)+2][innerRow*3+1], " ", board[(boardRow*3)+2][innerRow*3+2])
+                if (boardRow < 2 and innerRow == 2):
+                    print("-----------+-------------+-------------")
 
 def heuristic(board):
     print("not implemented")
@@ -15,7 +21,8 @@ def getSuccessors(board, move):
     assert(0)
 
 def simulateMove(board, whichMiniGame, whosTurn, depth, alpha, beta):
-    
+    print("not implemented")
+    assert(0)
 
 def playGames():
     opponentsTurn = False
@@ -23,6 +30,6 @@ def playGames():
         if( opponentsTurn == True ):
             #wait for command input from opponent
             assert(0)
-        else if(opponentsTurn == False ):
+        #else if (opponentsTurn == False ):
             #It's the computer's turn
-            assert(0)
+            #assert(0)
